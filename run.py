f2fd95641fd21875199ed38b01c94ed480c66367
Jamie Bennett <jbennett.130602@gmail.com>
2024-06-11 12:12:33 +0000

    get last 5 entries function

--- run.py
+++ run.py
@@ -2,16 +2,22 @@ import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
+
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
 
+
+
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+
 
 def get_sales_data():
     """
@@ -37,6 +43,7 @@ def get_sales_data():
     return sales_data    
 
 
+
 def validate_data(values):
     """
     Inside the try, converts all sring values into integers.
@@ -56,14 +63,6 @@ def validate_data(values):
     return True    
 
 
-#def update_sales_worksheet(data):
-#    """
-#    Update sales worksheet, add new row with the list data provided.
-#    """
-#    print('Updating sales worksheet...\n')
-#    sales_worksheet = SHEET.worksheet('sales')
-#    sales_worksheet.append_row(data)
-#    print('Sales worksheet updated successfully.\n')
 
 def calculate_surplus_data(sales_row):
     """
@@ -81,14 +80,24 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
     return surplus_data
 
-#def update_surplus_worksheet(data):
-#        """
-#        Update surplus worksheet, add new row with list data provided.
-#        """
-#        print('Updating surplus worksheet...\n')
-#        surplus_worksheet = SHEET.worksheet('surplus')
-#        surplus_worksheet.append_row(data)
-#        print('Surplus worksheet updated successfully.\n')
+
+
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet('sales')
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns    
+
+
 
 def update_worksheet(data, worksheet):
     """
@@ -99,6 +108,7 @@ def update_worksheet(data, worksheet):
     worksheet_to_upate = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f'{worksheet} worksheet updated successfully.\n')
+
 
     
 def main():
@@ -111,6 +121,9 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
 
+
+
 print('Welcome to Love Sandwiches data automation!')
-main()
+#main()
+sales_columns = get_last_5_entries_sales()
 
